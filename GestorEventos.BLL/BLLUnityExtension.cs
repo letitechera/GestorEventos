51bace3a81e250ae5c466ac61f8be7997a4d54eb
Leticia <leticiat@onetree.com>
2018-11-04 16:15:31 -0300

    UnityConfig

---
+++ GestorEventos.BLL/BLLUnityExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using GestorEventos.DAL;
+using Unity;
+using Unity.Extension;
+
+namespace GestorEventos.BLL
+{
+    public class BLLUnityExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            Container.AddNewExtension<DALUnityExtension>();
+
+            //Container.RegisterType<Inferface, Implementation>();
+        }
+    }
+}
---
+++ GestorEventos.DAL/DALUnityExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Unity;
+using Unity.Extension;
+using Unity.Lifetime;
+
+namespace GestorEventos.DAL
+{
+    public class DALUnityExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            //Container.RegisterType<Inferface, Implementation>();
+        }
+    }
+}
