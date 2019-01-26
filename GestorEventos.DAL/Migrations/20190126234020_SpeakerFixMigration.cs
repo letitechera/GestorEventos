a055a00dc7ea69e8d1f26571bbed8725112068ae
Leticia Techera <leticia.tech@gmail.com>
2019-01-26 20:41:14 -0300

    Fixes

---
+++ GestorEventos.DAL/Migrations/20190126234020_SpeakerFixMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GestorEventos.DAL.Migrations
+{
+    public partial class SpeakerFixMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FisrtName",
+                table: "Speaker",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Speaker",
+                newName: "FisrtName");
+        }
+    }
+}
