d774853a15ce354dbeffb6b40e4dbff952dcc2d7
Leticia Techera <leticia.tech@gmail.com>
2019-01-30 23:30:36 -0300

    Events Fix

--- GestorEventos.Models/WebApiModels/EventUI.cs
+++ GestorEventos.Models/WebApiModels/EventUI.cs
@@ -12,21 +12,25 @@ namespace GestorEventos.Models.WebApiModels
         public string Description { get; set; }
         public string Image { get; set; }
         public string Location { get; set; }
+        public string Address { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
         public string Topic { get; set; }
         public string CreatedById { get; set; }
 
-        public EventUI(Event _event, string location, string topic)
+        public EventUI(Event _event)
         {
             Id = _event.Id;
             Name = _event.Name;
             Description = _event.Description;
             Image = _event.Image;
-            Location = location ?? "N/A";
+            Location = _event.Location.Name ?? "N/A";
+            Address = _event.Location.Address1 + 
+                (string.IsNullOrEmpty(_event.Location.Address2) 
+                ? "" : ", "+ _event.Location.Address2);
             StartDate = _event.StartDate;
             EndDate = _event.EndDate;
-            Topic = topic ?? "N/A";
+            Topic = _event.EventTopic.Name;
             CreatedById = _event.CreatedById;
         }
     }
